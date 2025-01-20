3e9a5540e77ef225a0a2186f350005883cbe7270
David Spörri <davepermen@hotmail.com>
2025-01-20 23:30:12 +0100

    now all platformio based

---
+++ lib/esp8266-waveshare-epd/examples/epd7in5_V2-demo/epd7in5_V2-demo.ino
@@ -0,0 +1,168 @@
+/* Includes ------------------------------------------------------------------*/
+#include "DEV_Config.h"
+#include "EPD.h"
+#include "GUI_Paint.h"
+#include "imagedata.h"
+#include <stdlib.h>
+
+/* Entry point ----------------------------------------------------------------*/
+void setup()
+{
+    printf("EPD_7IN5_V2_test Demo\r\n");
+    DEV_Module_Init();
+
+    printf("e-Paper Init and Clear...\r\n");
+    EPD_7IN5_V2_Init();
+    EPD_7IN5_V2_Clear();
+    // DEV_Delay_ms(500);
+
+#if 1   // show image for array   
+    printf("show image for array\r\n");
+    EPD_7IN5_V2_Display(gImage_7in5_V2);
+    // DEV_Delay_ms(2000);
+
+#elif 0   // Drawing on the image
+    //Create a new image cache
+    EPD_7IN5_V2_Init_Fast();  
+    UBYTE *BlackImage;
+    /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
+    UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0) ? (EPD_7IN5_V2_WIDTH / 8 ) : (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
+    if ((BlackImage = (UBYTE *)malloc(Imagesize / 2)) == NULL) {
+        printf("Failed to apply for black memory...\r\n");
+        while (1);
+    }
+    printf("Paint_NewImage\r\n");
+    Paint_NewImage(BlackImage, EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT / 2, 0, WHITE);
+
+    //1.Select Image
+    printf("SelectImage:BlackImage\r\n");
+    Paint_SelectImage(BlackImage);
+    Paint_Clear(WHITE);
+
+    // 2.Drawing on the image
+    printf("Drawing:BlackImage\r\n");
+    Paint_DrawPoint(10, 80, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
+    Paint_DrawPoint(10, 90, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
+    Paint_DrawPoint(10, 100, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
+    Paint_DrawLine(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawLine(70, 70, 20, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawRectangle(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+    Paint_DrawRectangle(80, 70, 130, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+    Paint_DrawCircle(45, 95, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+    Paint_DrawCircle(105, 95, 20, WHITE, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+    Paint_DrawLine(85, 95, 125, 95, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+    Paint_DrawLine(105, 75, 105, 115, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+    Paint_DrawString_EN(10, 0, "waveshare", &Font16, BLACK, WHITE);
+    Paint_DrawString_EN(10, 20, "hello world", &Font12, WHITE, BLACK);
+    Paint_DrawNum(10, 33, 123456789, &Font12, BLACK, WHITE);
+    Paint_DrawNum(10, 50, 987654321, &Font16, WHITE, BLACK);
+    Paint_DrawString_CN(130, 0, " 你好abc", &Font12CN, BLACK, WHITE);
+    Paint_DrawString_CN(130, 20, "微雪电子", &Font24CN, WHITE, BLACK);
+
+    printf("EPD_Display\r\n");
+    EPD_7IN5_V2_SendHalfImage(0, BlackImage);
+    EPD_7IN5_V2_SendHalfImage(1, BlackImage);
+    EPD_7IN5_V2_TurnOnDisplay();
+    // DEV_Delay_ms(2000);
+
+    free(BlackImage);
+    BlackImage = NULL;
+#elif 0 
+    //Partial refresh, example shows time
+    // EPD_7IN5_V2_ClearBlack();
+    EPD_7IN5_V2_Init_Part();
+    UBYTE *BlackImage;
+    /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
+    UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0) ? (EPD_7IN5_V2_WIDTH / 8 ) : (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
+    if ((BlackImage = (UBYTE *)malloc(Imagesize / 2)) == NULL) {
+        printf("Failed to apply for black memory...\r\n");
+        while (1);
+    }
+    Paint_NewImage(BlackImage, Font20.Width * 7, Font20.Height, 0, BLACK);
+    Debug("Partial refresh\r\n");
+    Paint_SelectImage(BlackImage);
+    Paint_Clear(BLACK);
+	
+    PAINT_TIME sPaint_time;
+    sPaint_time.Hour = 12;
+    sPaint_time.Min = 34;
+    sPaint_time.Sec = 56;
+    UBYTE num = 10;
+    for (;;) {
+        sPaint_time.Sec = sPaint_time.Sec + 1;
+        if (sPaint_time.Sec == 60) {
+            sPaint_time.Min = sPaint_time.Min + 1;
+            sPaint_time.Sec = 0;
+            if (sPaint_time.Min == 60) {
+                sPaint_time.Hour =  sPaint_time.Hour + 1;
+                sPaint_time.Min = 0;
+                if (sPaint_time.Hour == 24) {
+                    sPaint_time.Hour = 0;
+                    sPaint_time.Min = 0;
+                    sPaint_time.Sec = 0;
+                }
+            }
+        }
+        Paint_ClearWindows(0, 0, Font20.Width * 7, Font20.Height, BLACK);
+        Paint_DrawTime(0, 0, &sPaint_time, &Font20, BLACK, WHITE);
+
+        num = num - 1;
+        if(num == 0) {
+            break;
+        }
+		    EPD_7IN5_V2_Display_Part(BlackImage, 150, 80, 150 + Font20.Width * 7, 80 + Font20.Height);
+        DEV_Delay_ms(500);//Analog clock 1s
+    }
+/*
+    The feature will only be available on screens sold after 24/10/23
+*/
+#else // show image for array
+    UBYTE *BlackImage;
+    UDOUBLE Imagesize = ((EPD_7IN5_V2_WIDTH % 4 == 0)? (EPD_7IN5_V2_WIDTH / 4 ): (EPD_7IN5_V2_WIDTH / 4 + 1)) * EPD_7IN5_V2_HEIGHT;
+    if((BlackImage = (UBYTE *)malloc(Imagesize/4)) == NULL) {
+        printf("Failed to apply for black memory...\r\n");
+        while (1);
+    }
+    EPD_7IN5_V2_Init_4Gray();
+    printf("4 grayscale display\r\n");
+    Paint_NewImage(BlackImage, EPD_7IN5_V2_WIDTH/2, EPD_7IN5_V2_HEIGHT/2, 0, WHITE);
+    Paint_SetScale(4);
+    Paint_Clear(0xff);
+    
+    Paint_DrawPoint(10, 80, GRAY4, DOT_PIXEL_1X1, DOT_STYLE_DFT);
+    Paint_DrawPoint(10, 90, GRAY4, DOT_PIXEL_2X2, DOT_STYLE_DFT);
+    Paint_DrawPoint(10, 100, GRAY4, DOT_PIXEL_3X3, DOT_STYLE_DFT);
+    Paint_DrawLine(20, 70, 70, 120, GRAY4, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawLine(70, 70, 20, 120, GRAY4, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawRectangle(20, 70, 70, 120, GRAY4, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+    Paint_DrawRectangle(80, 70, 130, 120, GRAY4, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+    Paint_DrawCircle(45, 95, 20, GRAY4, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+    Paint_DrawCircle(105, 95, 20, GRAY2, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+    Paint_DrawLine(85, 95, 125, 95, GRAY4, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+    Paint_DrawLine(105, 75, 105, 115, GRAY4, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+    Paint_DrawString_EN(10, 0, "waveshare", &Font16, GRAY4, GRAY1);
+    Paint_DrawString_EN(10, 20, "hello world", &Font12, GRAY3, GRAY1);
+    Paint_DrawNum(10, 33, 123456789, &Font12, GRAY4, GRAY2);
+    Paint_DrawNum(10, 50, 987654321, &Font16, GRAY1, GRAY4);
+    Paint_DrawString_CN(130, 0,"你好abc", &Font12CN, GRAY4, GRAY1);
+    Paint_DrawString_CN(130, 20,"你好abc", &Font12CN, GRAY3, GRAY2);
+    Paint_DrawString_CN(130, 40,"你好abc", &Font12CN, GRAY2, GRAY3);
+    Paint_DrawString_CN(130, 60,"你好abc", &Font12CN, GRAY1, GRAY4);
+    Paint_DrawString_CN(10, 130, "微雪电子", &Font24CN, GRAY1, GRAY4);
+    EPD_7IN5_V2_WritePicture_4Gray(BlackImage);
+    DEV_Delay_ms(3000);
+#endif
+
+  printf("Clear...\r\n");
+  EPD_7IN5_V2_Init();
+  EPD_7IN5_V2_Clear();
+
+  printf("Goto Sleep...\r\n");
+  EPD_7IN5_V2_Sleep();
+}
+
+/* The main loop -------------------------------------------------------------*/
+void loop()
+{
+  //
+}
