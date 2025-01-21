0f970e87dd2c3a190bf7f0815ba13212e61ceda8
David Spörri <davepermen@hotmail.com>
2025-01-22 00:21:02 +0100

    seems to work, polls every 5s

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,19 @@
 #include <WiFiClient.h>
 #include "config.h"
 
+uint8_t *imageBits;
+
 void setup() {
+  imageBits = (uint8_t*)malloc(imageSize);
+}
+
+void loop() {
   WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
   }
-  uint8_t *imageBits = (uint8_t*)malloc(imageSize);
   WiFiClient wificlient;
   HTTPClient http;
   if(http.begin(wificlient, webrequest) == false) {
@@ -31,17 +36,11 @@ void setup() {
   wificlient.stop();
   http.end();
   WiFi.disconnect();
-  
+
   DEV_Module_Init();
   EPD_1IN54_V2_Init();
-  EPD_1IN54_V2_Clear();
-  //DEV_Delay_ms(500);
   EPD_1IN54_V2_Display(imageBits);
-  //DEV_Delay_ms(1000);
   EPD_1IN54_V2_Sleep();
-  free(imageBits);
-  imageBits = NULL;
-}
-
-void loop() {
+  
+  delay(5*60e3);
 }
