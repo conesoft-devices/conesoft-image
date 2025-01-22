49fc597cf6ce0fe25f1bd3003758f6bd8158575e
David Spörri <davepermen@hotmail.com>
2025-01-23 00:30:12 +0100

    image update with new wifi settings dialog work

--- src/config.h
+++ src/config.h
@@ -1,9 +1,3 @@
-extern const char* ssid;
-extern const char* password;
-extern const char* resource;
-extern const char* server;
-extern const int port;
-
 extern const char* webrequest;
 
 const int imageSize = (200 * 200) / 8;
--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,39 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <DoubleResetDetector.h>
+#include <WiFiSettings.h>
+#include <LittleFS.h>
 #include "config.h"
+
+DoubleResetDetector drd(10, 0);
 
 uint8_t *imageBits;
 
 void setup() {
+  system_update_cpu_freq(160);
+  Serial.begin(115200);
+  Serial.println("booting up conesoft-web-image");
+  pinMode(LED_BUILTIN, OUTPUT);
+  LittleFS.begin();
+
+  WiFiSettings.hostname = "csft-img-";
+  String host = WiFiSettings.string("host", "", "Conesoft Web Devices Server");
+  int port = WiFiSettings.integer("port", 0, 65535, 0, "Conesoft Web Devices Server Port");
+
+  if (drd.detectDoubleReset()) {
+    WiFiSettings.portal();
+  }
+
+  WiFiSettings.connect();
+
   imageBits = (uint8_t*)malloc(imageSize);
 }
 
+unsigned long time_now = 0;
+unsigned long period = 60 * 1000;
+
 void loop() {
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-  }
   WiFiClient wificlient;
   HTTPClient http;
   if(http.begin(wificlient, webrequest) == false) {
@@ -41,6 +59,13 @@ void loop() {
   EPD_1IN54_V2_Init();
   EPD_1IN54_V2_Display(imageBits);
   EPD_1IN54_V2_Sleep();
-  
-  delay(5*60e3);
+
+  time_now = millis();
+
+  while(millis() < time_now + period) {
+    delay(10);
+    drd.loop();
+  }
+
+  ESP.restart();
 }
