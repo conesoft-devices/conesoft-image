476ccd81185d1af3d2c428de5172ace5f4a34fc5
David Spörri <davepermen@hotmail.com>
2025-01-23 16:31:30 +0100

    now

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,7 @@
 
 String webRequest;
 uint8_t *imageBits;
-
 const int imageSize = (200 * 200) / 8;
-
-unsigned long time_now = 0;
-unsigned long period = 60 * 1000;
 
 void setup()
 {
@@ -21,23 +17,15 @@ void setup()
 
 void loop()
 {
-
   csft_web_request(webRequest, "Conesoft-Web-Image", [](HTTPClient &http) -> void
-  {
-    csft_binary_read_response_to(http, imageBits, imageSize);
-  });
+                   { csft_binary_read_response_to(http, imageBits, imageSize); });
 
   DEV_Module_Init();
   EPD_1IN54_V2_Init();
   EPD_1IN54_V2_Display(imageBits);
   EPD_1IN54_V2_Sleep();
 
-  time_now = millis();
-  while (millis() < time_now + period)
-  {
-    delay(10);
-    csft_loop();
-  }
+  csft_loop_for(60 * 1000);
 
   ESP.restart();
 }
